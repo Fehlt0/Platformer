5a24b977b222fac828ee190845d511f8e8732a96
vincent Pandit <vincentpandit9@gmail.com>
2026-05-25 17:06:59 +0200

    la luciole revient
    donc si on rappuie dessus apres l'avoir lance elle revient, elle n'active pas les plantes sur le retour !

--- Assets/Scripts/LaunchLuciole.cs
+++ Assets/Scripts/LaunchLuciole.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,10 +8,14 @@ public class LaunchLuciole : MonoBehaviour
     public GameObject luciolePrefab;
     public Transform firePoint;
     public float launchSpeed = 10f;
+    public float returnSpeed = 20f;
 
     private Vector2 aimDirection;
     
     private GameObject currentLuciole;
+    private bool lucioleAlreadyLaunched = false;
+    
+
 
 
     public void Update()
@@ -21,9 +26,20 @@ public class LaunchLuciole : MonoBehaviour
 
     public void OnLaunch(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && !lucioleAlreadyLaunched)
         {
+
+            lucioleAlreadyLaunched = true;
             Launch();
+            
+            
+        }
+        else if (context.performed && lucioleAlreadyLaunched)
+        {
+
+            lucioleAlreadyLaunched = false;
+            GoBack();
+            
         }
     }
 
@@ -39,11 +55,6 @@ public class LaunchLuciole : MonoBehaviour
 
         Vector2 direction = aimDirection.normalized;
         
-        if (currentLuciole != null)
-        {
-            Destroy(currentLuciole);
-        }
-
         currentLuciole = Instantiate(luciolePrefab, firePoint.position, Quaternion.identity);
 
         
@@ -55,6 +66,47 @@ public class LaunchLuciole : MonoBehaviour
         if (script != null)
         {
             script.direction = direction;
+        }
+    }
+    private void GoBack()
+    {
+        if (currentLuciole == null) return;
+
+        Rigidbody2D rb = currentLuciole.GetComponent<Rigidbody2D>();
+        Luciole script = currentLuciole.GetComponent<Luciole>();
+        CircleCollider2D collider = currentLuciole.GetComponent<CircleCollider2D>();
+
+        rb.bodyType = RigidbodyType2D.Dynamic;
+
+        if (script != null)
+        {
+            script.enabled = false;
+        }
+
+        if (collider != null)
+        {
+            collider.enabled = false;
+        }
+
+        StartCoroutine(ReturnToPlayer(rb));
+    }
+
+    private IEnumerator ReturnToPlayer(Rigidbody2D rb)
+    {
+        while (currentLuciole != null)
+        {
+            Vector2 directionToPlayer = firePoint.position - currentLuciole.transform.position;
+
+            if (directionToPlayer.magnitude < 0.2f)
+            {
+                Destroy(currentLuciole);
+                currentLuciole = null;
+                break;
+            }
+
+            rb.linearVelocity = directionToPlayer.normalized * returnSpeed;
+
+            yield return null; 
         }
     }
 }
