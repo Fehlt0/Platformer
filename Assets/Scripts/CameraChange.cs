cb4c6039b1e29606bdfdba04c7d314dea48061e3
Jean-Baptiste Delbove <deljeanbap@gmail.com>
2026-06-04 11:58:58 +0200

    camera bloque le joueur

--- Assets/Scripts/CameraChange.cs
+++ Assets/Scripts/CameraChange.cs
@@ -3,12 +3,28 @@ using UnityEngine;
 
 public class CameraChange : MonoBehaviour
 {
+    private BoxCollider2D collider2D;
+
+    private void Start()
+    {
+        collider2D = GetComponent<BoxCollider2D>();
+    }
+
     [SerializeField] private int id;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             CameraManager.instance.SetNewTarget(id);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("gsrhgrshr");
+        if (other.CompareTag("Player"))
+        {
+            collider2D.isTrigger = false;
         }
     }
 }
