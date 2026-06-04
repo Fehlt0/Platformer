fd654eff8dce42b57f46d30bb87da5b667629bd4
Jean-Baptiste Delbove <deljeanbap@gmail.com>
2026-06-04 10:33:40 +0200

    PlantPlatform est active dès le début de l'anim

--- Assets/Scripts/Plant/PlantPlatform.cs
+++ Assets/Scripts/Plant/PlantPlatform.cs
@@ -2,4 +2,9 @@ using UnityEngine;
 
 public class PlantPlatform : Plant
 {
+    public override void IfIsAlive()
+    {
+        base.IfIsAlive();
+        gameObject.GetComponent<BoxCollider2D>().isTrigger = !isAlive;
+    }
 }
