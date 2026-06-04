63db6c1de755841f76c44bd588eb9e42a993ac56
Jean-Baptiste Delbove <deljeanbap@gmail.com>
2026-06-04 10:41:14 +0200

    ok mtn la plateforme elle est clean as fuck

--- Assets/Scripts/Plant/PlantPlatform.cs
+++ Assets/Scripts/Plant/PlantPlatform.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class PlantPlatform : Plant
 {
+    private BoxCollider2D _collider2d;
+
+    public override void Start()
+    {
+        base.Start();
+        _collider2d = gameObject.GetComponent<BoxCollider2D>();
+    }
+
     public override void IfIsAlive()
     {
         base.IfIsAlive();
-        gameObject.GetComponent<BoxCollider2D>().isTrigger = !isAlive;
+        _collider2d.isTrigger = !isAlive;
     }
 }
