32ef4cbffc34e7b7d5a760205b01b10ca2fd5df4
Mael <m.barreira@rubika-edu.com>
2026-06-09 10:57:47 +0200

    Fix de l'audio manager

--- Assets/Scripts/Manager/AudioManager.cs
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -7,13 +7,13 @@ public class AudioManager : MonoBehaviour
     
     private void Awake()
     {
-        if (instance == null)
+        if (instance != null)
         {
-            instance = this;
+            Destroy(this);
         }
         else
         {
-            Destroy(this);
+            instance = this;
         }
         
     }
